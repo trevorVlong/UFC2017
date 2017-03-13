ccdc35a0a9c2289c630c7d2d32ecb50e2f43cf28
trevorVlong <longt@mit.edu>
2017-03-13 14:03:16 -0400

    Update Plane_Vanilla.m

--- Plane_Vanilla.m
+++ Plane_Vanilla.m
@@ -3,13 +3,13 @@
 %Payload
     str = load('Master_Constant.mat');
         %contains all constants from primary doc, can be changed
-    [AR, S, cavg] = ARSspace(Str.c_function, str.b);
+    [AR, S, cavg] = ARSspace(str.c_function, str.b);
         %finds Aspect Ratio, Planform area, and average chord length
     [W_wing,W_body] = Weighing(str.rho, cavg, str.tau, str.b, str.W_fuse);
         %finds Wing weight and Plane body weight
         
     %%%% Make a new function to create the parts for this
-    W_payd = ((((str.CDA_0)/S)/(str.C_L))+(str.c_d)/(str.C_L)+(Str.C_L)/(pi*AR*str.e));
+    W_payd = ((((str.CDA_0)/S)/(str.C_L))+(str.c_d)/(str.C_L)+(str.C_L)/(pi*AR*str.e));
     
     
     W_pay = str.T_max/(W_payd)-W_body;
@@ -23,17 +23,39 @@
     [delta, M0, I0] = WingBend(str.E,str.tau,str.epsilon,str.c_t,str.c_r,str.b,str.N,str.W_fuse,W_pay);  
         %gets Moment, Bending inertia, and deflection of wingtip
 
-    d_span = double(delta/Str.b); 
+    d_span = double(delta/str.b); 
         %gets tip-deflection to wingspan ratio
     
 %Constrained Back-solving
 
 %Bending-Constrained d_span2 >= .1 C_L = .8
 
-    C_L2 = .8; 
-        %lift coefficient
-    d_spand2 = .1 ; 
+    d_spand = .1 ; 
         %tip deflection ratio
-    [Wpay_tc,delta2,M02] = defweight(I0,1,d_spand2,Str.b,Str.lambda,Str.W_fuse);
+    [Wpay_tc1,delta1,M01] = defweight(I0,1,d_spand,str.b,str.lambda,str.W_fuse);
         %returns bending-constrained payload, tip deflection, and Moment
+    d_spand2 = .05;
+    [Wpay_tc2,delta2,M02] = defweight(I0,1,d_spand2,str.b,str.lambda,str.W_fuse);
+        %bending-constrained payload for delta/b_max = .05
+    d_spand3 = .15;
+    [Wpay_tc3,delta3,M03] = defweight(I0,1,d_spand3,str.b,str.lambda,str.W_fuse);
+        %bending-constrained payload for delta/b_max = .05
+        
+        
+        
+% bending constrained time to turn in a 12.5m circle (delta/b = .1, Wpay =
+% 0 C_L = .8
+ 
+    R = 12.5; %meters
+    Wpay_tr = 0;
+        %payload weight for plane
+    W_plane = W_wing+W_body+Wpay_tr;
+    
+        %Total plane weight
+    trev = double(RevTime(W_plane,S,str.C_L,R,str.T_max));
+        %revolution time
+    
+    
+    
+    
     
